85927ea5963e0798bfb9e9610f7d07d62f507efe
Vansh Malhotra <s24cseu1302@bennett.edu.in>
2025-08-24 16:07:42 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -202,7 +202,7 @@ model, medications_df, symptoms = load_data()
 st.markdown(
     """
     <div class='app-header'>
-        <div class='title-icon'>⚜🩺</div>
+        <div class='title-icon'>🩺</div>
         <h1>Diagnox AI</h1>
         <p>Your personal AI health companion for intelligent symptom analysis and preliminary insights.</p>
     </div>
@@ -288,5 +288,6 @@ else:
 # Footer
 # ----------------------------
 st.markdown("<p class='footer'>DiagnoX AI &copy; 2025 | Developed by Vansh</p>", unsafe_allow_html=True)
+
 
 
